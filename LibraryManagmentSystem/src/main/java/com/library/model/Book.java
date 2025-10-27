38394c8efd50f1fc63f1254b348c19ccdd66dfb4
ipsa03 <pratibimbitaipsa@gmail.com>
2025-10-27 17:37:32 +0530

    add method to insert featured book

--- LibraryManagmentSystem/src/main/java/com/library/model/Book.java
+++ LibraryManagmentSystem/src/main/java/com/library/model/Book.java
@@ -1,5 +1,22 @@
 package com.library.model;
 
-public class Book {
+public class Book { 
+	private String title ;
+	private String genre ;
+	private int authorId;
+	private boolean featured;
+	
+	public Book (String title,String genre,int authorId,boolean featured) {
+		this.title = title;
+		this.authorId = authorId;
+		this.genre = genre;
+		this.featured = featured;
+	}
+	
+	public String getTitle() {return title;}
+	public String genre()  {return genre;}
+	public int authorId() {return authorId;}
+	public boolean featured() {return featured;}
+	
 
 }
